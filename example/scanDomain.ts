45d06c72b9a88e424e9c027cb54e28c152cd2b06
Randy Pen <pzm16@tsinghua.org.cn>
2024-10-31 11:11:19 +0800

    tab in example

--- example/scanDomain.ts
+++ example/scanDomain.ts
@@ -1,13 +1,13 @@
-import {SuiSecBlocklist} from "suisecblocklist";
+import { SuiSecBlocklist } from "suisecblocklist";
 
 async function main() {
 
     const urls=[
         "https://sui.io",
         "https://app.cetus.zone/swap",
-        "https://scam-cetus.zone/swap",//block
-        "https://scam.scam-cetus.zone/swap",//block
-        "https://500-airdrop.top",//block
+        "https://scam-cetus.zone/swap",         //block
+        "https://scam.scam-cetus.zone/swap",    //block
+        "https://500-airdrop.top",              //block
     ]
     const blocklist = new SuiSecBlocklist();
     await blocklist.fetchDomainlist();
