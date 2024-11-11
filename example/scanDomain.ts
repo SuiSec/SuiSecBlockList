aef54751c81bca95a2010c4c34f482c3b471f248
SurfWallet <contact@surf.tech>
2024-11-11 17:53:20 +0800

    Fix scan domain endsWith "/"

--- example/scanDomain.ts
+++ example/scanDomain.ts
@@ -4,14 +4,22 @@ async function main() {
 
     const urls = [
         "https://sui.io",
+        "https://sui.io/",
         "deepbook.tech",
+        "deepbook.tech/",
         "https://app.cetus.zone/swap",
         "https://a1.b2.deepbook.cetus.zone",
         "https://deepbook.cetus.zone/v2",
         "https://scam-cetus.zone/swap",               //block
         "https://scam.scam-cetus.zone/swap",          //block
         "https://scam1.scam2.scam-cetus.zone/swap",   //block
-        "https://500-airdrop.top"                     //block
+        "https://scam-cetus.zone/",                   //block
+        "scam-cetus.zone/",                           //block
+        "https://scam-cetus.zone/",                   //block
+        "https://500-airdrop.top",                    //block
+        "https://500-airdrop.top/",                   //block
+        "500-airdrop.top",                            //block
+        "500-airdrop.top/",                           //block
     ]
     const blocklist = new SuiSecBlocklist();
     await blocklist.fetchDomainlist();
